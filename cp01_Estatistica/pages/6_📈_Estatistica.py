c0606f37f8c3747d4ae7d3b3f2a3be98e127c294
Eduardo Mazelli <eduardo.mazelli@gmail.com>
2025-03-10 14:57:00 -0300

    Update nas páginas do CV

--- cp01_Estatistica/pages/6_📈_Estatistica.py
+++ cp01_Estatistica/pages/6_📈_Estatistica.py
@@ -4,8 +4,12 @@ import numpy as np
 from streamlit_extras.app_logo import add_logo
 
 st.set_page_config(page_title="Formação e Experiências", layout="wide")
+
 st.logo("crowdstrike.png")
+
 st.sidebar.markdown("Desenvolvido por Eduardo Mazelli ®️ 2025")
 col1, col2 = st.sidebar.columns([0.5, 0.5])
 col1.markdown("""<a href="https://github.com/GiantMazelliWasHere"><img src="https://cdn3.emoji.gg/emojis/6705-githubblack.png" width="64px" height="64px" alt="github"></a>""", unsafe_allow_html=True)
 col2.markdown("""<a href="https://www.linkedin.com/in/eduardo-mazelli/"><img src="https://cdn3.emoji.gg/emojis/70322-linkedin.png" width="64px" height="64px" alt="LinkedIn"></a>""""", unsafe_allow_html=True)
+
+st.image("estatistica.png", width=150)
