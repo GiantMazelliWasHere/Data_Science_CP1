c2c7835c2be1690b4a2f73f4887b03c26cde031a
Eduardo Mazelli <eduardo.mazelli@gmail.com>
2025-03-12 02:34:54 +0000

    AJuste no logo

--- cp01_Estatistica/1_👨🏻‍💼_Home.py
+++ cp01_Estatistica/1_👨🏻‍💼_Home.py
@@ -5,7 +5,7 @@ from streamlit_extras.app_logo import add_logo
 
 st.set_page_config(page_title="Eduardo Mazelli", layout="wide")
 
-st.logo("images/logo.png")
+st.logo("logo.png")
 
 st.sidebar.markdown("Desenvolvido por Eduardo Mazelli ®️ 2025")
 
